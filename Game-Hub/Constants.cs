1b873561c053334b734875a03e8e524410912f56
brunosas88 <bruno.sas@live.com>
2023-01-16 17:35:33 -0300

    Adding dictionary readonly and translate methods
    Dictionary and translation methods
    will serve to manipulate the position informed
    by the player and position understood by the program

--- Game-Hub/Constants.cs
+++ Game-Hub/Constants.cs
@@ -20,5 +20,15 @@ namespace Game_Hub
 		public const string LogMenuFirstOption = "Ranking";
 		public const string LogMenuSecondOption = "Histórico de Partidas";
 		public const string LogMenuBackToMainOption = "Voltar ao Menu Principal";
+		public static readonly Dictionary<string, int> LineReference = new Dictionary<string, int>
+		{			
+			{"8", 0}, {"7", 1}, {"6", 2}, {"5", 3}, {"4", 4}, {"3", 5}, {"2", 6}, {"1", 7}
+		};
+		public static readonly Dictionary<string, int> ColumnReference = new Dictionary<string, int>
+		{
+			{"a", 0}, {"b", 1}, {"c", 2}, {"d", 3}, {"e", 4}, {"f", 5}, {"g", 6}, {"h", 7}			
+		};
+
 	}
+		
 }
--- Game-Hub/Utils.cs
+++ Game-Hub/Utils.cs
@@ -52,6 +52,22 @@ namespace Game_Hub
 			}
 		}
 
+		public static int[] GetRealPosition(string position)
+		{			
+			int line = Constants.LineReference[position[0].ToString()];
+			int column = Constants.ColumnReference[position[1].ToString()];
+			int[] realPosition = {line, column};
+			return realPosition;
+		}
+
+		public static string NominatePosition(int line, int column)
+		{
+			string nominatedLine = Constants.LineReference.First(entries => entries.Value == line).Key;
+			string nominatedColumn = Constants.ColumnReference.First(entries => entries.Value == column).Key;
+		
+			return nominatedLine + nominatedColumn;
+		}
+
 
 	}
 }
