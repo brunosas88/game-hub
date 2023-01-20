d70b758794d28bbdecc75a2043918a58a069ba6d
brunosas88 <bruno.sas@live.com>
2023-01-20 17:03:34 -0300

    Adding new class and enum
    GameTitle enum will be responsible to
    keep the name of games in hub
    MatchEvaluation will represent info
    about the matches of each game

---
+++ Game-Hub/Model/MatchEvaluation.cs
@@ -0,0 +1,38 @@
+﻿using Game_Hub.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Game_Hub.Model
+{
+	public class MatchEvaluation
+	{
+		public GameTitle Game { get; set; }
+		public int Points { get; set; }
+		public int Victories { get; set; }
+		public int Draws { get; set ; }
+		public int Defeats { get; set; }		
+
+		[JsonConstructor]
+		public MatchEvaluation(GameTitle game, int points, int victories, int draws, int defeats)
+		{
+			Game = game;
+			Points = points;
+			Victories = victories;
+			Draws = draws;
+			Defeats = defeats;
+		}
+
+		public MatchEvaluation(GameTitle game) 
+		{
+			Game = game;
+			Points = 0;
+			Victories = 0;
+			Draws= 0;
+			Defeats = 0;
+		}
+	}
+}
