dcd03727d94fe15d63639879f14b051ba71236e9
brunosas88 <bruno.sas@live.com>
2023-01-26 21:54:51 -0300

    Adding the battleship board
    The board will be set at 10x10 and composed
    by the enum BattleShip FieldInfo that will
    provide necessary info about each field on
    the board

---
+++ Game-Hub/Model/BattleShip/BattleShipBoard.cs
@@ -0,0 +1,49 @@
+﻿using Game_Hub.Model.Chess;
+using Game_Hub.Model.Enums;
+using Game_Hub.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Hub.Model.BattleShip
+{
+	public class BattleShipBoard
+	{
+		private BattleShipFieldInfo[,] _board = new	BattleShipFieldInfo[10,10];
+
+		public BattleShipFieldInfo[,] Board { get => _board; }
+
+		public BattleShipBoard()
+		{
+			CreateBoard();
+		}
+
+		public void UpdateBoard(List<BattleShipFieldInfo> fields)
+		{
+			int row, col;
+			foreach (BattleShipFieldInfo field in fields)
+			{
+				col = Constants.COLUMN_REFERENCE[field.Position[0].ToString()];
+				row = int.Parse(field.Position[1].ToString()) - 1;
+
+				_board[row, col] = field;
+			}
+		}
+
+		private void CreateBoard()
+		{
+			BattleShipFieldInfo cleanField = new();
+			cleanField.IsShot = false;
+			cleanField.IsShip = false;
+			cleanField.Position = string.Empty;
+			for (int row = 0; row < _board.GetLength(0); row++)
+			{
+				for (int col = 0; col < _board.GetLength(1); col++)
+					_board[row, col] = cleanField;
+			}
+		}
+
+	}
+}
---
+++ Game-Hub/Model/Enums/BattleShipFieldInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Hub.Model.Enums
+{
+	public struct BattleShipFieldInfo
+	{
+		public bool IsShot { get; set; }
+		public bool IsShip { get; set; }
+		public string Position { get; set; }
+	}
+}
