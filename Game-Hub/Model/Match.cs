55a27060e9d092b9393f0626043638723c06b060
brunosas88 <bruno.sas@live.com>
2023-02-02 14:52:00 -0300

    Adding date play information in match

--- Game-Hub/Model/Match.cs
+++ Game-Hub/Model/Match.cs
@@ -8,14 +8,18 @@ namespace Game_Hub.Model
 		public GameTitle Game { get; set; }
 		public string PlayerOne { get; set; }
 		public string PlayerTwo { get; set; }
+		public DateTime InitialPlayTime { get; set; }
+		public DateTime FinalPlayTime { get; set; }
 		public int PlayerOneVictories { get; set; }
 		public int PlayerTwoVictories { get; set; }
 		public int Draws { get; set; }
 		public int MatchesPlayed { get { return PlayerOneVictories + PlayerTwoVictories + Draws; } }
 
 		[JsonConstructor]
-		public Match(GameTitle game, string playerOne, string playerTwo, int playerOneVictories, int playerTwoVictories, int draws) : this(game, playerOne, playerTwo)
+		public Match(GameTitle game, string playerOne, string playerTwo, DateTime initialPlayTime, DateTime finalPlayTime, int playerOneVictories, int playerTwoVictories, int draws) : this(game, playerOne, playerTwo)
 		{
+			InitialPlayTime = initialPlayTime;
+			FinalPlayTime = finalPlayTime;
 			PlayerOneVictories = playerOneVictories;
 			PlayerTwoVictories = playerTwoVictories;
 			Draws = draws;
